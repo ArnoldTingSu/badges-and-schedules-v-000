629108222db68ce32c54bc72f90a46159385ab33
Arnold Su <arnoldtingsu@gmail.com>
2018-10-13 07:02:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = [1,2,3,4,5,6,7]
+  
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speaker)
+  speaker.map do |speaker|
+    "Hello, my name is #{speaker}."
+  end
+end
+
+def assign_rooms(speaker)
+  speaker.map do |speaker|
+    
